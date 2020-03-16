27027d13687c9a0ae04c7e2c3170f716cd28cc09
avbravo <avbravo@avbravo>
2020-03-16 15:35:12 -0500

    con jmoordbutils 0.65

--- src/main/java/com/bpbonline/sendweb/FileController.java
+++ src/main/java/com/bpbonline/sendweb/FileController.java
@@ -11,7 +11,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.Serializable;
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -25,27 +25,51 @@ import javax.ws.rs.core.Response;
  */
 @Named
 @ViewScoped
-public class FileController implements Serializable{
-    
-    
-    public String send(){
-  
-             try {
-//			File file = new File("D:/Documents/JavaJ2EE.doc");
-			File file = new File("/home/avbravo/Descargas/license.json");
-			InputStream fileInStream = new FileInputStream(file);
-                        
-			String contentDisposition = "attachment; filename=\"" + file.getName() + "\"";
-			Client client = ClientBuilder.newClient();
-			Response response = client.target("http://localhost:8080/serverfiles/resources/file").path("upload").request()
-					.header("Content-Disposition", contentDisposition)
-					.post(Entity.entity(fileInStream, MediaType.APPLICATION_OCTET_STREAM));
-			System.out.println(response.readEntity(String.class));
-                       JsfUtil.infoDialog("enviado", "Se envio el archivo exitosamente");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-                        JsfUtil.errorDialog("send)=", e.getLocalizedMessage());
-		}
+public class FileController implements Serializable {
+
+    private String directory = JsfUtil.userHome() + JsfUtil.fileSeparator() + "fiscalprinter" + JsfUtil.fileSeparator() + "license" + JsfUtil.fileSeparator();
+
+    public String send() {
+
+        try {
+            File fileEnc = new File(directory + "license.enc");
+
+            File fileIvEnc = new File(directory + "licenseiv.enc");
+
+            InputStream fileInStream = new FileInputStream(fileEnc);
+            InputStream fileInStreamIvEnc = new FileInputStream(fileIvEnc);
+
+            String contentDispositionFileEnc = "attachment; filename=\"" + fileEnc.getName() + "\"";
+            String contentDispositionFileIvEnc = "attachment; filename=\"" + fileIvEnc.getName() + "\"";
+
+            Client client = ClientBuilder.newClient();
+            Response response = client.target("http://localhost:8080/serverfiles/resources/file").path("upload").request()
+                    .header("Content-Disposition", contentDispositionFileEnc)
+                    .post(Entity.entity(fileInStream, MediaType.APPLICATION_OCTET_STREAM));
+            if (response.getStatus() == 200) {
+                JsfUtil.infoDialog(fileEnc.getName(), "Se envio el archivo exitosamente");
+               /**
+                * 
+                * Envia el archivo encriptado
+                */
+                 Client clientIvEnc = ClientBuilder.newClient();
+               Response   responseIvEnc = clientIvEnc.target("http://localhost:8080/serverfiles/resources/file").path("upload").request()
+                        .header("Content-Disposition", contentDispositionFileIvEnc)
+                        .post(Entity.entity(fileInStreamIvEnc, MediaType.APPLICATION_OCTET_STREAM));
+                if (responseIvEnc.getStatus() == 200) {
+                    JsfUtil.infoDialog(fileIvEnc.getName(), "Se envio el archivo exitosamente");
+                } else {
+                    JsfUtil.infoDialog(fileIvEnc.getName(), "No se envio el archivo. Status:" + responseIvEnc.getStatus());
+                }
+
+            } else {
+                JsfUtil.infoDialog(fileEnc.getName(), "No se envio el archivo. Status:" + response.getStatus());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            JsfUtil.errorDialog("send)=", e.getLocalizedMessage());
+        }
         return "";
     }
 }
